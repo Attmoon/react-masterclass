2d5a0be2d0f618d29319bc7393783c207a33fefc
Attmoon <99157555+Attmoon@users.noreply.github.com>
2025-07-01 08:11:08 +0900

    'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -11,13 +11,25 @@ const Box = styled.div`
 const Circle = styled(Box)`
   border-radius: 50px;
 `;
-
+const Btn = styled.button`
+color: white;
+background-color: tomato;
+border: 0;
+border-radius: 15px;
+`;
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
 
 function App() {
   return  (
-    <Father>
-      <Box bgColor="teal"/>
-      <Circle bgColor="tomato"/>
+    <Father as="header">
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
     </Father>
   );
 }
